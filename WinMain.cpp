d6b889130636453dc1ce4fb8af50b38dc453dc72
Ewan <StrikerKagami@Gmail.com>
2021-07-11 12:43:31 +0100

    Custom Windows Procedure + exit code handling
    Implemented a Windows Procedure, suitable for expansion later down the line. So far, it just terminates the program when the close button is pressed. 
    
    Also added support for error codes from the messages.

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,5 +1,15 @@
 #include <windows.h>
 
+/* A new Windows Procedure. This is how we'll be handling WinAPI messages. */
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {   
+
+    switch (msg) {
+    case WM_CLOSE:              //Windows messages are all macroed, so you don't need to remember each code.
+        PostQuitMessage(11);    //Tells our application to terminate execution with a custom exit code.
+        break;
+    }
+    return(DefWindowProc(hWnd, msg, wParam, lParam));
+}
 
 /*Classic program entry point. Not the one we want to be using, however.*/
 /*
@@ -29,17 +39,17 @@ int CALLBACK WinMain(
     WNDCLASSEX wc = { 0 };      //Create a Window Class to be registered. Good practice to 0 out configuration structures.
     wc.cbSize = sizeof(wc);     //The size of the struct in bytes.
     wc.style = CS_OWNDC;        //The style of the window class. see https://docs.microsoft.com/en-us/windows/win32/winmsg/window-class-styles for more. OR these to combine.
-    wc.lpfnWndProc = DefWindowProc; //Pointer to the window procedure. This is used in message handling. see https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633573(v=vs.85) 
+    wc.lpfnWndProc = WndProc;   //Pointer to the windows procedure. This is used in message handling. see https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms633573(v=vs.85) 
     wc.cbClsExtra = 0;          //How many extra bytes to allocate to the window class struct. 0 by default.
     wc.cbWndExtra = 0;          //Same as above, but for the Window instance. 
     wc.hIcon = nullptr;         //A handle to an icon resource. If null, uses the system default
     wc.hCursor = nullptr;       //A handle to a custom cursor resource. If null, will use the system default.
-    wc.hbrBackground = nullptr;
+    wc.hbrBackground = nullptr; //Background Resource.
     wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    wc.hIconSm = nullptr;
+    wc.lpszClassName = pClassName;  //The name of our window class. Must be constant across all calls. 
+    wc.hIconSm = nullptr;           
 
-    RegisterClassEx(&wc);
+    RegisterClassEx(&wc);      //Registers our new Window Class with the operating system. 
 
 
     //2. Create the window instance
@@ -56,11 +66,21 @@ int CALLBACK WinMain(
 
 
     //4. Evaluate Messages
+    BOOL gResult;
     MSG msg; //An instance of the Windows API Message struct. See https://docs.microsoft.com/en-us/windows/win32/api/winuser/ns-winuser-msg
-    while (GetMessage(&msg, nullptr, 0, 0) > 0) { 
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0) { 
         TranslateMessage(&msg);
         DispatchMessageW(&msg);
     }
 
-    return 0;
+    if (gResult == -1) {
+        return -1;
+    }
+    else {
+        return msg.wParam;
+    }
+    //return 0;
 }
+
+
+
